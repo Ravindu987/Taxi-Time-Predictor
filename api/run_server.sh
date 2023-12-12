7a23aa3a55499653575a7fba8f0b3aed99031469
Ravindu <ravindumadawalagama@gmail.com>
2023-12-12 19:03:49 +0530

    Bentoml config fix

--- api/run_server.sh
+++ api/run_server.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 # Run the server
-BENTOML_CONFIG="/media/ravindu/Ravindu/Projects/Taxi Time Predictor/api/bentoml_configuration.yaml" bentoml serve create_service.py
+BENTOML_CONFIG="./bentoml_configuration.yaml" bentoml serve create_service.py
